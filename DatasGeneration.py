4266856b92e486fb81d76c9b32b3fef5878f4da5
SURFACE <rgf.blondel@gmail.com>
2023-10-25 18:20:21 +0200

    classifieur binaire pour la classe Specification

--- DatasGeneration.py
+++ DatasGeneration.py
@@ -47,7 +47,7 @@ class FileReader:
         for example in pdtb:
             section = int(example['Section'])
             if example['Relation'] == 'Implicit' and section in split_sec2set:
-                gold_class = example['ConnHeadSemClass1'].split('.')[0]
+                gold_class = 'Specification' if 'Specification' in example['ConnHeadSemClass1'].split('.')[0] else 'Autre'
                 self.Arg1[split_sec2set[section]].append(example['Arg1_RawText'])
                 self.Arg2[split_sec2set[section]].append(example['Arg2_RawText'])
                 self.y[split_sec2set[section]].append(gold_class)
