8af89ebde6229095e7c9fdb3999885cd63e76072
SURFACE <rgf.blondel@gmail.com>
2023-10-30 12:10:37 +0100

    hp

--- main.py
+++ main.py
@@ -93,15 +93,15 @@ discourse_relation_mlp = discourse_relation_mlp.to(device)
 
 # choix de l'optimizer (SGD, Adam, Autre ?)
 optim = torch.optim.Adam(discourse_relation_mlp.parameters(),
-                         lr=0.00005,
-                         weight_decay=0.00007)
+                         lr=0.0001,
+                         weight_decay=0.0002)
 
 # entrainement
 dev_losses, train_losses = discourse_relation_mlp.training_step(optimizer=optim,
                                                                 nb_epoch=1000,
                                                                 patience=2,
                                                                 down_sampling=True,
-                                                                size_of_samples=2500,
+                                                                size_of_samples=3000,
                                                                 fixed_sampling=False)
 
 
